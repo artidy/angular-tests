ce820ced3ce0394e1480687feb63453000c73b66
Andrey Pechennikov <ap@devandy.me>
2021-02-24 17:21:30 +0600

    create integrated tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { PostsComponent } from './posts/posts.component';
+import { RoutingComponent } from './routing/routing.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ColorDirective } from './directives/color.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    PostsComponent,
+    RoutingComponent,
+    NavbarComponent,
+    ColorDirective
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
